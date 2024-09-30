737db066971dc7b8c89c5a28ff5940bbfd1591a8
Roman Zagrebin <seenyat@me.com>
2024-10-01 01:55:49 +0200

    fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { createSignal } from "solid-js";
-import solidLogo from "./assets/solid.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Presentation from "./components/Slider";
 
